32ffd0942bc3d65497ccfe244ecda1e054852dc9
VojislavM <milivojevicvoja@yahoo.com>
2016-12-16 15:47:32 +0100

    checksum crc32 testing

--- lib_test.ino
+++ lib_test.ino
@@ -1,6 +1,8 @@
 
 #include "message.hpp"
 #include "frame.hpp"
+#include "crc32.hpp"
+#include "inet.hpp"
 
 void setup() {
   Serial.begin(115200);
@@ -22,8 +24,23 @@ void loop() {
   //Serial.println(sizeof(msg));
   //message_tlv_add_command(&msg, COMMAND_MOVE_MOTOR);
   //message_tlv_add_reply(&msg, REPLY_ERROR_REPORT);
-  //tlv_motor_position_t position = {100, 100, 0};
+  //tlv_motor_position_t position = {0, 0, 0};
   //message_tlv_add_motor_position(&msg, &position);
+
+  uint8_t buffer[1024];
+  size_t length = message_serialize(buffer, 1024, &msg);
+  Serial.print("Message length: ");
+  Serial.println(length);
+  Serial.println("Serialized protocol message:");
+  for (size_t i = 0; i < length; i++) {
+    Serial.print(buffer[i], HEX);
+    //Serial.write(buffer[i]);
+    Serial.print(" ");
+  }
+  //printf("\n");
+  Serial.println();
+  
+  
   //message_tlv_add_power_reading(&msg, 0x0444);
   //tlv_error_report_t error_report;
   //error_report.code = 35;
@@ -38,12 +55,12 @@ void loop() {
   ssize_t test_frame_size;
   
   Serial.println("Generated protocol message: ");
-  //message_print(&msg);
+  message_print(&msg);
  
   Serial.println("\n");
   
-  uint8_t buffer[1024];
-  size_t length = message_serialize(buffer, 1024, &msg);
+  //uint8_t buffer[1024];
+ /* size_t*/ length = message_serialize(buffer, 1024, &msg);
   Serial.print("Message length: ");
   Serial.println(length);
   Serial.println("Serialized protocol message:");
@@ -55,7 +72,7 @@ void loop() {
   //printf("\n");
   Serial.println();
   
-  test_frame_size = frame_message(test_frame, sizeof(test_frame), &msg);
+  //test_frame_size = frame_message(test_frame, sizeof(test_frame), &msg);
   Serial.print("Serialized protocol message with frame:");
   Serial.println();
 /*  for (size_t i = 0; i < test_frame_size; i++) {
@@ -66,6 +83,26 @@ void loop() {
 
   message_free(&msg);
 
+
+  uint8_t byteBuffer[] = "Hello World";
+  uint8_t *value = byteBuffer;
+
+  uint32_t checksum = 0;
+  checksum = crc32(checksum, value, (size_t)sizeof(byteBuffer));
+    //Serial.println(checksum);
+
+  Serial.println(checksum, HEX);
+
+  checksum = htonl(checksum);
+
+  Serial.println(checksum, HEX);
+
   Serial.println("end");
   while(1);
 }
+/*
+GET_STATUS:
+F1 01 00 01 01 03 00 04 A5 05 DF 1B F2
+    1  0  1  1  3  0  4  8  0  0  0 
+
+*/

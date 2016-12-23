9c764985e5f87c5938992b90f7da17981eef09a6
VojislavM <milivojevicvoja@yahoo.com>
2016-12-23 16:58:31 +0100

    main test code

--- lib_test.ino
+++ lib_test.ino
@@ -1,111 +1,106 @@
 
 #include "message.hpp"
 #include "frame.hpp"
-//#include "crc32.hpp"
 #include "inet.hpp"
 
-//#include <SoftwareSerial.h>
-
-//String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;  // whether the string is complete
-
+/*** RECEIVING DATA VARIABLES ***/
+/* True when receiving string is completed */
+boolean stringComplete = false;
+boolean send_frame_bool = false;
+/* Receiving buffer for incomming serial data */
 volatile uint8_t rx_buffer[100];
+/* Temperary receiving buffer for incomming serial data*/
 uint8_t rx_data[2];
+/* Number of bytes received on serial */
 int rx_indx;
-volatile int transfer_cplt;
+/* Check buffer for receiving serual data */
 uint8_t rx_last[2] = {0x00, 0x00};
+/* Final length of serial received data */
 int message_len = 0;
 
-  uint8_t test_frame[1024];
-  ssize_t test_frame_size;
-
-  /* Parsed message */
-  message_t msg_parsed;
-  
-//  // put your main code here, to run repeatedly:
-//  /* Generate message - test message */
-  message_t msg;
-//SoftwareSerial mySerial(3, 2); // RX, TX
 
 void setup() {
+  /*Serial for communication */
   Serial.begin(115200);
+  /* Serial test */
   Serial.println("begin");
-
-  // reserve 200 bytes for the inputString:
-  //inputString.reserve(200);
-   // set the data rate for the SoftwareSerial port
-//  mySerial.begin(9600);
-//  mySerial.println("Hello, world?");
 }
 
 void loop() {
+    /* Parsed message */
+    message_t msg_parsed;
+    
+    /*** TRANSMITTING DATA VARIABLES ***/
+    /* Sending frame buffer */
+    uint8_t send_frame[100];
+    /* Sending frame size */
+    ssize_t send_frame_size;
+    /* Sending message */
+    message_t msg_send; 
 
-  message_init(&msg);
-//  //Serial.print("msg sizeof: ");
-//  //Serial.println(sizeof(msg));
-  message_tlv_add_command(&msg, COMMAND_GET_STATUS);
-//  //Serial.print("msg sizeof: ");
-//  //Serial.println(sizeof(msg));
-//  message_tlv_add_command(&msg, COMMAND_MOVE_MOTOR);
-//  //message_tlv_add_reply(&msg, REPLY_ERROR_REPORT);
-//  tlv_motor_position_t position = {1000, 0, 0};
-//  message_tlv_add_motor_position(&msg, &position);
-//  //message_tlv_add_power_reading(&msg, 0x0444);
-//  //tlv_error_report_t error_report;
-//  //error_report.code = 35;
-//  //message_tlv_add_error_report(&msg, &error_report);
-//  //Serial.print("msg.length ");
-//  //Serial.println(msg.length);
-  message_tlv_add_checksum(&msg);
-//  //Serial.print("msg sizeof: ");
-//  //Serial.println(sizeof(msg));
-//  
-////  Serial.println("Generated protocol message: ");
-////  message_print(&msg);
-//  
-//  //Serial.println();
-//  
-//  test_frame_size = frame_message(test_frame, sizeof(test_frame), &msg);
-////  Serial.print("Serialized protocol message with frame:");
-////  Serial.println();
-////  for (size_t i = 0; i < test_frame_size; i++) {
-////    Serial.print(test_frame[i], HEX);
-////    Serial.print(" ");
-////  }
-////  Serial.println();
-//
-  message_free(&msg);
+    send_frame_bool = true;
 
-  //Serial.println("end");
-  //delay(1000);
-  //while(1){
-    if (stringComplete) {
-      //Serial.println(inputString);
+  while(true){
+    if(send_frame_bool){
+      /* Init for sending message */
+      message_init(&msg_send);
+      /* Uncomment to add values to the message */
+       message_tlv_add_command(&msg_send, COMMAND_GET_STATUS);
+    // message_tlv_add_command(&msg_send, COMMAND_MOVE_MOTOR);
+    // message_tlv_add_reply(&msg_send, REPLY_ERROR_REPORT);
+    // tlv_motor_position_t position = {1000, 0, 0};
+    // message_tlv_add_motor_position(&msg_send, &position);
+    //  message_tlv_add_power_reading(&msg_send, 0x0444);
+    //  tlv_error_report_t error_report;
+    //  error_report.code = 35;
+    //  message_tlv_add_error_report(&msg_send, &error_report);
+      message_tlv_add_checksum(&msg_send);
+      /* Print generated message */
+      Serial.println("Generated protocol message: ");
+      message_print(&msg_send);  
+      Serial.println();
+  
+      send_frame_size = frame_message(send_frame, sizeof(send_frame), &msg_send);
+      Serial.print("Serialized protocol message with frame:");
+      Serial.println();
+      for (size_t i = 0; i < send_frame_size; i++) {
+        Serial.print(send_frame[i], HEX);
+        Serial.print(" ");
+      }
+      Serial.println();
+      /* Free generated message */
+      message_free(&msg_send);
+
+      send_frame_bool = false;
+    }
+    /* When new frame is received on serial, previously generated message will be sent to serial*/
+    /* and received message will be decoded and value sent to serial.*/
+    if (stringComplete){
+      /* Received serail message */
       Serial.println("Received serialized protocol message:");
       for (size_t i = 0; i < message_len; i++) {
         Serial.print(rx_buffer[i], HEX);
         Serial.print(" ");
       }
       Serial.println();
-
-      Serial.print("message_len: ");
+  
+      Serial.print("Received message_len: ");
       Serial.println(message_len);
       
-        //message_init(&msg_parsed);
-//      //parse received message
-        int num = frame_parser((uint8_t *)&rx_buffer, message_len, &msg_parsed);
-        //Serial.print("number: ");
-        Serial.println(num);
-        Serial.println("Parsed protocol message: ");
-        message_print(&msg_parsed);
-//      Serial.println();
-        message_free(&msg_parsed); 
-      // clear the string:
-      //inputString = "";
+  
+      /* Parse received message */
+      int num = frame_parser((uint8_t *)&rx_buffer, message_len, &msg_parsed);
+      Serial.println("Parsed protocol message: ");
+      message_print(&msg_parsed);
+      Serial.println();
+      /* Free received message */
+      message_free(&msg_parsed); 
       stringComplete = false;
-    }
-  //}
-}
+      
+    }//end if
+    serialEvent();
+  }//while
+}//end loop()
 
 
 /*
@@ -150,7 +145,7 @@ void serialEvent() {
         message_len = rx_indx;
         rx_indx=0;
         /* Transfer complete, data is ready to read */
-        stringComplete = true; //Transfer_cplt=1;
+        stringComplete = true;
         /* Disable USART1 interrupt */
         //HAL_NVIC_DisableIRQ(USART1_IRQn);
       }
@@ -165,6 +160,7 @@ void serialEvent() {
     
   }
 }
+
 /*
 GET_STATUS:
 F1 01 00 01 01 03 00 04 A5 05 DF 1B F2
